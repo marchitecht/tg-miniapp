9b291760e5d6a0dc1d37441a7b9098815563dd73
Marat Musin <maratmusin@MacBook-Pro-Marat.local>
2025-01-14 17:23:27 +0300

    rm imps

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './App.css';
 import { Calendar } from './components/CalendarView/Calendar';
 import { formatDate } from './components/CalendarView/utils';
-// import { getMonthNames } from "./components/CalendarView/utils/getMonthNames";
-// import { Stepper } from "./components/Stepper/Stepper";
 import styles from './components/CalendarView/Calendar.module.scss';
 
 import { useInitData } from '@vkruglikov/react-telegram-web-app';
